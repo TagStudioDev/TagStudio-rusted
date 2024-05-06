aa7338612bd1cca9391955eb0fe18f9b1ffa138e
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-05-06 10:54:40 -0500

    Backend: remove unnecessary unit type

--- src-tauri/src/command/library.rs
+++ src-tauri/src/command/library.rs
@@ -1,7 +1,7 @@
 use tauri::api::dialog::blocking::FileDialogBuilder;
 
 #[tauri::command]
-pub async fn library_open() -> () {
+pub async fn library_open() {
     let paths = FileDialogBuilder::new()
         .set_title("Select Libraries")
         .pick_folders();
